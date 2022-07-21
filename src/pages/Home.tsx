57744c2eb782b985db4ae05fa2f7f4882a605d01
leopego <leonardopegomartins@gmail.com>
2022-07-21 16:41:20 -0300

    App done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,53 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Pressable, StyleSheet, View, Text } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  interface Task {
+    id: number;
+    title: string;
+    done: boolean;
+  }
+
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
+
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, task]);
+
+    console.log(tasks);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map((task) => ({ ...task }));
+
+    const foundTask = updatedTasks.find((task) => task.id === id);
+
+    if (!foundTask) return;
+
+    foundTask.done = !foundTask.done;
+
+    setTasks(updatedTasks);
+
+    console.log(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(updatedTasks);
   }
 
   return (
@@ -25,19 +55,24 @@ export function Home() {
       <Header tasksCounter={tasks.length} />
 
       <TodoInput addTask={handleAddTask} />
-
-      <TasksList 
-        tasks={tasks} 
+      {/* <Pressable
+        onPress={() => handleToggleTaskDone(1658421227484)}
+        style={{ width: 200, height: 50, backgroundColor: "red" }}
+      >
+        <Text>Leonardo ASDASD</Text>
+      </Pressable> */}
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
